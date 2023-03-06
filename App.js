173279b3351808a564b279452a9f555378815522
Ibrahim  Habib <58874664+Millz147@users.noreply.github.com>
2023-03-06 10:13:26 +0100

    restructured

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
+import React from 'react';
 import { useFonts } from 'expo-font';
 import { fonts } from './src/features/constants';
 import { store } from './src/infrastructures/redux/store';
 import { Provider } from 'react-redux';
-import React, { useState } from 'react';
 import { Navigation } from './src/infrastructures/navigation';
 
 export default function App() {
